a3c91658f9a8c85f18919c8ba3183ad18acc3695
Boris Moreno <boris_marco_moreno@hotmail.com>
2022-05-06 13:26:45 -0500

    COntact form fix

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,7 +4,7 @@ const ContactForm = () => {
     return ( 
         <div className='container mx-auto my-20 w-full xl:w-1/3 md:w-1/2 border border-primary bg-white'>
             <div className='p-5 space-y-5 shadow-xl'>
-                <form method='post' action='https://api.factura-agil.com/api/empresa/contacto'>
+                <form method='post' action='https://api.chat-ecuador.com/api/empresa/contacto'>
                     <div className='grid grid-cols-2 gap-5'>
                         <input 
                             className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-primary-lighter'
