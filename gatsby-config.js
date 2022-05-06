e86e05a002a3ea17561369dc0984dd5a7db696dd
Boris Moreno <boris_marco_moreno@hotmail.com>
2022-05-05 21:16:08 -0500

    Gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,20 @@ module.exports = {
         icon: `src/images/invoice-icon-2.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-228110300-1", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
